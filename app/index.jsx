854f195bebc37949f0da1512d4fdef648ac731f5
Dayana <dayana.tquispe@gmail.com>
2026-03-01 10:30:52 -0300

    Primeiro commit do projeto React Native

---
+++ app/index.jsx
@@ -0,0 +1,72 @@
+import { Image, StyleSheet, Text, View, Pressable } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./pomodoro.png")} />
+      <View style={styles.actions}>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.button}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais.
+        </Text>
+        <Text style={styles.footerText}>Desenvolvido por Alura.</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+
+  // actions porque vão ser as ações do pomodoro.
+  actions: {
+    paddingVertical: 24,
+    paddingHorizontal: 24,
+    backgroundColor: "#14448080", // adicionamos os digitos 80 para opacidade da borda
+    width: "80%",
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  button: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 8,
+  },
+
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+  },
+
+  footer: {
+    width: "80%",
+  },
+
+  footerText: {
+    textAlign: "center",
+    color: "#98A0A8",
+    fontSize: 12.4,
+  },
+});
